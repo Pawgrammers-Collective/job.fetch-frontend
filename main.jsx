0ef89cd6dbb3b0bc3f96de0c542fd958a6b9c796
JohnnyBackus <john.p.backus@gmail.com>
2023-12-18 10:56:26 -0800

    :deleted redundant App file

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Auth0Provider} from '@auth0/auth0-react'
-import App from './App.jsx'
+import App from './src/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
