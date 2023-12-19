f686d5fd5ef31be83f0552c83ea1de5a69fa035d
Xin Deng <xind1470@gmail.com>
2023-12-19 02:18:19 -0800

    made new page for first site render

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h2>Welcome to Job.fetch( )</h2>
+      <p>Please log in to access the full content and features of the site.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
