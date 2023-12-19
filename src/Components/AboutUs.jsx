9945f90b3f7b0612fdcc74c117c32894b9b29921
Xin Deng <xind1470@gmail.com>
2023-12-19 11:48:33 -0800

    Update AboutUs.jsx

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import aboutUs from '../assets/AboutUs.json';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+// import styles from './aboutus.module.css'
 
 function AboutUs() {
+  const [selectedPerson, setSelectedPerson] = useState(null);
+
+  const handleCardClick = (person) => {
+    setSelectedPerson(person);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedPerson(null);
+  };
+
   return (
     <>
+        <h3>Click on an image to learn more</h3>
+    <div className='about-container'>
       {aboutUs.map((value, index) => (
-        <Card key={index} style={{ width: '18rem' }}>
-          <Card.Img style={{height: '100px', width: '100px'}}variant="top" src={value.img} alt={value.name} />
+        <Card
+          key={index}
+          className="about-card" 
+          onClick={() => handleCardClick(value)}
+        >
+          <Card.Img
+            className="profile-img" 
+            variant="top"
+            src={value.img}
+            alt={value.name}
+          />
           <Card.Body>
             <Card.Title>{value.name}</Card.Title>
-            <Card.Text>{value.description}</Card.Text>
           </Card.Body>
         </Card>
       ))}
+      </div>
+
+      <Modal show={selectedPerson !== null} onHide={handleCloseModal}>
+        <Modal.Header >
+          <Modal.Title>{selectedPerson && selectedPerson.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{selectedPerson && selectedPerson.description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
 
 export default AboutUs;
+
