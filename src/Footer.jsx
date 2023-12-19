5e027f7fd6938b1647ace3151f55690cbf7ec4b3
Xin Deng <xind1470@gmail.com>
2023-12-19 00:53:52 -0800

    changed footer class to function

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>&copy; Pawgrammers Collective</Navbar.Brand>
-      </Navbar>
-    )
-  }
-}
+const Footer = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand>&copy; Pawgrammers Collective</Navbar.Brand>
+    </Navbar>
+  );
+};
 
 export default Footer;
