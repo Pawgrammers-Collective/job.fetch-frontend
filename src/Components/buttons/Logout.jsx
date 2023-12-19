27295af5b4fec7b7335dd7dbeeb7ecca76f36964
Xin Deng <xind1470@gmail.com>
2023-12-19 09:44:39 -0800

    fixed small merge issue

--- src/Components/buttons/Logout.jsx
+++ src/Components/buttons/Logout.jsx
@@ -3,7 +3,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
   const { isAuthenticated, logout } = useAuth0();
-  const { isAuthenticated, logout } = useAuth0();
 
   function handleLogout() {
     console.log("Logging out...");
