d9318e8e8fe89ab2cd3c73e70b3010ace26d8c91
Xin Deng <xind1470@gmail.com>
2023-12-19 02:26:47 -0800

    changed home to include letter, job info and added authentication

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,34 @@
-function Home () {
+import React from 'react';
+import CoverLetter from './CoverLetter';
+import JobAccordian from './JobAccordian';
+import JobCard from './JobCard';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function Home() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <>
-    <h1>Home</h1>
+      <h1>Home</h1>
+
+      {isAuthenticated && (
+        <>
+       
+          <JobAccordian />
+
+         
+          <JobCard />
+
+
+          <CoverLetter />
+        </>
+      )}
+
+      {!isAuthenticated && (
+        <p>Login to view content</p>
+      )}
     </>
-  )
+  );
 }
 
 export default Home;
