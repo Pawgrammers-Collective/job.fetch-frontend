284c279fc99920d980016bfb759bf33c492978a3
JohnnyBackus <john.p.backus@gmail.com>
2023-12-20 14:42:05 -0800

    updated save button to disable after saving

--- src/Components/JobCard.jsx
+++ src/Components/JobCard.jsx
@@ -1,29 +1,40 @@
 
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import styles from "./styles/JobCard.module.css";
 
 function JobCard(props) {
-  console.log(props);
+  const [isSaved, setIsSaved] = useState(false);
 
   return props.job.map((value, index) => {
     return (
       <div key={index}>
-        <Card className={styles.Card}style={{ width: "40em", height: "18rem" }}>
+        <Card className={styles.Card} style={{ width: "40em", height: "18rem" }}>
           {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
           <Card.Body>
             <Card.Title>{value.title}</Card.Title>
             <Card.Text>{value.description}</Card.Text>
 
-             <Button 
+            <Button
               variant="primary"
               type="submit"
-              onClick = {() => props.handleSave (value)} >Save this Job! </Button>
+              onClick={() => {
+                props.handleSave(value);
+                setIsSaved(true);
+              }}
+              className={`${styles.saveButton} ${isSaved ? styles.savedButton : ""}`}
+              disabled={isSaved}
+            >
+              {isSaved ? "Saved!" : "Save this Job!"}
+            </Button>
 
-            <Button onClick= {()=>props.onSaveCoverLetter(value.title, value.description)}
-            variant="primary">Generate a Cover Letter!</Button>
-
+            <Button
+              onClick={() => props.onSaveCoverLetter(value.title, value.description)}
+              variant="primary"
+            >
+              Generate a Cover Letter!
+            </Button>
           </Card.Body>
         </Card>
       </div>
