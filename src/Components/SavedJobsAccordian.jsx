210192f28645f84779231ed3940ac135fae0f399
JohnnyBackus <john.p.backus@gmail.com>
2023-12-19 18:29:41 -0800

    accordians within accordians

--- src/Components/SavedJobsAccordian.jsx
+++ src/Components/SavedJobsAccordian.jsx
@@ -7,22 +7,54 @@ function SavedJobsAccordian(props) {
   console.log(props.savedJobs);
   return (
     <>
-    <Accordion defaultActiveKey="0">
-      {props.savedJobs.map((value, idx) => {
-        return (
-          <>
-          <Accordion.Item eventKey={idx}>
-            <Accordion.Header>{value.title}</Accordion.Header>
-            <Accordion.Body>
-              {value.description}
-            </Accordion.Body>
-          </Accordion.Item>
-          </>
-        )
-      })}
-    </Accordion>
+      <Accordion defaultActiveKey={['0']} alwaysOpen>
+        {props.savedJobs.map((value, idx) => {
+          return (
+            <>
+              <Accordion.Item eventKey={idx}>
+                <Accordion.Header>{value.jobData.title}</Accordion.Header>
+                <Accordion.Body>
+                  <Accordion.Item eventKey={`${idx}.${idx}`}>
+                    <Accordion.Header>Job Description</Accordion.Header>
+                    <Accordion.Body>
+                      {value.jobData.description}
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  {/* <Accordion.Item eventKey={`${idx}.${idx}.${idx}`}>
+                    <Accordion.Header>Job Qualifications</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                      {value.jobData.qualifications.items.map((value, idx) => {
+                        return (
+                          <li key={idx}> {value} </li>
+                        )
+                        }
+                      )}
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item> */}
+                  {/* <Accordion.Item eventKey={`${idx}.${idx}.${idx}.${idx}`}>
+                    <Accordion.Header>Job Responsibilities</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                      {value.jobData.responsibilities.items.map((value, idx) => {
+                        return (
+                          <li key={idx}> {value} </li>
+                        )
+                        }
+                      )}
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item> */}
+                </Accordion.Body>
+              </Accordion.Item>
+            </>
+          );
+        })}
+      </Accordion>
     </>
   );
 }
+
 
 export default SavedJobsAccordian;
