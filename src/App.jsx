a603baca9b1d35606266cb66c95b3e06864a11a0
Kyle Aleman <kylealeman18@gmail.com>
2023-12-21 01:37:30 +0000

    auth0 functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // App.jsx
 
 import React, { useState, useEffect } from 'react';
@@ -10,8 +11,9 @@ import AboutUs from './Components/AboutUs';
 import UserProfile from './Components/UserProfile.jsx';
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
-import LandingPage from './Components/LandingPage'; 
+import LandingPage from './Components/LandingPage';
 import SearchForm from './Components/SearchForm.jsx';
+import { withAuth0 } from '@auth0/auth0-react';
 
 const url = import.meta.env.VITE_SERVER_URL;
 
@@ -26,130 +28,189 @@ function App(props) {
   const [interviewQuestion, setInterviewQuestion] = useState('');
 
   async function handleSaveCoverLetter(jobTitle, jobDescription) {
-    try {
-      const response = await axios.get(`${url}/cover`, {
+    if (props.auth0.isAuthenticated) {
+      let claim = await props.auth0.getIdTokenClaims();
+      console.log(claim)
+      let token = claim.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "get",
+        url: `${url}/cover`,
+        // If this doesn't work prob the params
         params: {
           jobTitle,
           jobDescription,
-        },
+        }
       }
-      );
-      setCoverLetters(response.data);
-console.log('Cover letter get', response);
-
-    } catch (error) {
-      console.error('Error generating cover letter:', error);
-    }
-  }
-
-  async function getInterviewQuestions(request, response){
-    try {
-      response = await axios.get(`${url}/interview`);
-      console.log(response.data);
-      // setInterviewQuestion(response.data)
-    } catch(e){
-      console.log('frontend interview questions no worky :(', e)
-    }
-  }
-
-
-  function handleSearch(searchInput) {
-    console.log(searchInput);
-    setCity(searchInput);
-    getJobs(searchInput);
-  }
-
-  async function getJobs(city) {
-    console.log(url);
-    try {
-      let response = await axios.get(`${url}/jobs?location=${city}`);
-      console.log(response.data);
-      setJobs(response.data);
-    } catch (error) {
-      console.error('Error getting jobs:', error.message);
-    }
-  }
-
-
-  function handleSave(job) {
-    console.log(job);
-    // setSavedJobs(job);
-    saveJob(job);
-  }
-
-  async function saveJob(job){
-    console.log(job);
       try {
-        let response = await axios.post(`${url}/jobs`, job);
-        console.log("Server Response", response.data);
-        // setSavedJobs(... savedJobs , response.data);
+        const response = await axios(config);
+        setCoverLetters(response.data);
+        console.log('Cover letter get', response);
+
       } catch (error) {
-        console.error(error.message);
+        console.error('Error generating cover letter:', error);
+      }
+    }}
+
+    async function getInterviewQuestions(request, response) {
+      if (props.auth0.isAuthenticated) {
+        let claim = await props.auth0.getIdTokenClaims();
+        console.log(claim)
+        let token = claim.__raw;
+        const config = {
+          headers: { "Authorization": `Bearer ${token}` },
+          method: "get",
+          url: `${url}/interview`,
+        }
+        try {
+          response = await axios(config);
+          console.log(response.data);
+          // setInterviewQuestion(response.data)
+        } catch (e) {
+          console.log('frontend interview questions no worky :(', e)
+        }
       }
     }
 
-  async function getSavedJobs(){
-    try {
-      let response = await axios.get(`${url}/jobs/saved`);
-      console.log(response.data);
-      setSavedJobs(response.data);
-    } catch(error) {
-        console.error('Error getting jobs:', error.message);
+
+    function handleSearch(searchInput) {
+      console.log(searchInput);
+      setCity(searchInput);
+      getJobs(searchInput);
     }
-  }
 
-  async function deleteSavedJob(job){ 
-    console.log('deleting ',{job});
-    try {
-      let response = await axios.delete(`${url}/jobs/${job._id}`);
-      console.log(response.data);
-      getSavedJobs();
-    } catch(error) {
-        console.error('Error deleting jobs:', error.message);
+    async function getJobs(city) {
+      console.log(url);
+      if (props.auth0.isAuthenticated) {
+        let claim = await props.auth0.getIdTokenClaims();
+        console.log(claim)
+        let token = claim.__raw;
+        const config = {
+          headers: { "Authorization": `Bearer ${token}` },
+          method: "get",
+          url: `${url}/jobs?location=${city}`
+        }
+        try {
+          let response = await axios(config);
+          console.log(response.data);
+          setJobs(response.data);
+        } catch (error) {
+          console.error('Error getting jobs:', error.message);
+        }
+      }
     }
-  }
 
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
 
-          <Route
-            exacts
-            path="/"
-            element={<LandingPage />}
-          />
+    function handleSave(job) {
+      console.log(job);
+      // setSavedJobs(job);
+      saveJob(job);
+    }
 
-          <Route
-            exact path="/Home"
+    async function saveJob(job) {
+      console.log(job);
+      if (props.auth0.isAuthenticated) {
+        let claim = await props.auth0.getIdTokenClaims();
+        console.log(claim)
+        let token = claim.__raw;
+        const config = {
+          headers: { "Authorization": `Bearer ${token}` },
+          method: "post",
+          url: `${url}/jobs`,
+          data: job
+        }
+        try {
+          let response = await axios(config);
+          console.log("Server Response", response.data);
+          // setSavedJobs(... savedJobs , response.data);
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+    }
 
-            element={<Home 
-              jobs={jobs}
+    async function getSavedJobs() {
+      if (props.auth0.isAuthenticated) {
+        let claim = await props.auth0.getIdTokenClaims();
+        console.log(claim)
+        let token = claim.__raw;
+        const config = {
+          headers: { "Authorization": `Bearer ${token}` },
+          method: "get",
+          url: `${url}/jobs/saved`,
+        }
+        try {
+          let response = await axios(config);
+          console.log(response.data);
+          setSavedJobs(response.data);
+        } catch (error) {
+          console.error('Error getting jobs:', error.message);
+        }
+      }
+    }
 
-               onSaveCoverLetter={handleSaveCoverLetter} coverLetters={coverLetters} handleSave = {handleSave} handleSearch={handleSearch} getQuestions={getInterviewQuestions}
+    async function deleteSavedJob(job) {
+      console.log('deleting ', { job });
+      if (props.auth0.isAuthenticated) {
+        let claim = await props.auth0.getIdTokenClaims();
+        console.log(claim)
+        let token = claim.__raw;
+        const config = {
+          headers: { "Authorization": `Bearer ${token}` },
+          method: "delete",
+          url: `${url}/jobs/${job._id}`,
+        }
+        try {
+          let response = await axios(config);
+          console.log(response.data);
+          getSavedJobs();
+        } catch (error) {
+          console.error('Error deleting jobs:', error.message);
+        }
+      }
+    }
 
-               />}
-          />
-
-          <Route
-            exact path="/profile"
-            element={<UserProfile 
-              getSavedJobs = {getSavedJobs} 
-              savedJobs = {savedJobs}
-              deleteSavedJob = {deleteSavedJob}/>}
-           />
+    return (
+      <>
+        <Router>
+          <Header />
+          <Routes>
 
             <Route
-            exact path="/about-us"
-            element={<AboutUs />}
-          />
+              exacts
+              path="/"
+              element={<LandingPage />}
+            />
 
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
-}
+            <Route
+              exact path="/Home"
 
-export default App;
+              element={<Home
+                jobs={jobs}
+
+                onSaveCoverLetter={handleSaveCoverLetter} coverLetters={coverLetters} handleSave={handleSave} handleSearch={handleSearch} getQuestions={getInterviewQuestions}
+
+              />}
+            />
+
+            <Route
+              exact path="/profile"
+              element={<UserProfile
+                getSavedJobs={getSavedJobs}
+                savedJobs={savedJobs}
+                deleteSavedJob={deleteSavedJob} />}
+            />
+
+            <Route
+              exact path="/about-us"
+              element={<AboutUs />}
+            />
+
+          </Routes>
+          <Footer />
+        </Router>
+      </>
+    );
+  }
+
+  export default withAuth0(App);
