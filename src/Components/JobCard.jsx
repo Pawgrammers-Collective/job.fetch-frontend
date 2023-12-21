41ad27993130bbcd7c71b29cd14d68089b7c39fe
Xin Deng <xind1470@gmail.com>
2023-12-21 06:59:32 -0800

    basic css

--- src/Components/JobCard.jsx
+++ src/Components/JobCard.jsx
@@ -1,4 +1,6 @@
+
 import React, { useState } from "react";
+
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import styles from "./styles/JobCard.module.css";
@@ -6,100 +8,72 @@ import styles from "./styles/JobCard.module.css";
 function JobCard(props) {
   const [isSaved, setIsSaved] = useState(Array(props.job.length).fill(false));
 
+
   const splitDescription = (description) => {
-    try {
-      if (!description) {
-        return [];
-      }
+    // Use a regular expression to find and replace the desired text with bold formatting
+    const formattedDescription = description.replace(
+      /\n\n(.*?)\n\n/g,
+      (match, group) => `\n\n**${group}**\n\n`
+    );
 
-      const formattedDescription = description.match(/\n\n([^\n]+?)\n\n/g);
-      let matchAll = Array.from(formattedDescription);
-      console.log("Formatted Description:", matchAll);
-
-      return matchAll.map((text, i) => {
-        console.log("Current match:", text);
-        return <h4 key={i}>{text}</h4>;
-      });
-    } catch (error) {
-      console.error('Error in splitDescription:', error.message);
-      return [];
-    }
+    return formattedDescription.split("\n\n");
   };
 
   const formatParagraph = (paragraph, index) => {
-    try {
-      if (typeof paragraph !== 'string') {
-        throw new Error('Invalid paragraph type');
-      }
-
-      const bulletPoints = paragraph.split("\n•");
-      console.log("Bullet Points:", bulletPoints);
-
-      if (bulletPoints.length > 1) {
-        return (
-          <div key={index}>
-            <p>{bulletPoints[0]}</p>
-            <ul>
-              {bulletPoints.slice(1).map((point, pointIndex) => (
-                <li key={pointIndex}>{point.trim()}</li>
-              ))}
-            </ul>
-          </div>
-        );
-      }
-
-      console.log("Returning from formatParagraph:", paragraph);
-      return <p key={index}>{paragraph}</p>;
-    } catch (error) {
-      console.error('Error in formatParagraph:', error.message);
-      return <p key={index}>Error in formatting paragraph</p>;
+    const bulletPoints = paragraph.split("\n•");
+    
+    if (bulletPoints.length > 1) {
+      return (
+        <div key={index}>
+          <p>{bulletPoints[0]}</p>
+          <ul>
+            {bulletPoints.slice(1).map((point, pointIndex) => (
+              <li key={pointIndex}>{point.trim()}</li>
+            ))}
+          </ul>
+        </div>
+      );
     }
+
+    return <p key={index}>{paragraph}</p>;
   };
-
-  const renderButtons = (value, index) => (
-    <>
-      <Button
-        variant="primary"
-        type="submit"
-        onClick={() => {
-          props.handleSave(value);
-          const updatedIsSaved = [...isSaved];
-          updatedIsSaved[index] = !updatedIsSaved[index];
-          setIsSaved(updatedIsSaved);
-        }}
-        className={`${styles.saveButton} ${
-          isSaved[index] ? styles.savedButton : ""
-        }`}
-        disabled={isSaved[index]}
-      >
-        {isSaved[index] ? "Saved!" : "Save this Job!"}
-      </Button>
-
-      <Button
-        onClick={() =>
-          props.onSaveCoverLetter(value.title, value.description)
-        }
-        variant="primary"
-      >
-        Generate a Cover Letter!
-      </Button>
-    </>
-  );
 
   return (
     <div className={styles.JobCardContainer}>
       {props.job.map((value, index) => (
-        <Card
-          key={index}
-          className={styles.Card}
-          style={{ width: "40em", height: "18rem" }}
-        >
+        <Card key={index} className={styles.Card} style={{ width: "40em", height: "18rem" }}>
+
           <Card.Body>
             <Card.Title>{value.title}</Card.Title>
 
-            {splitDescription(value.description)}
-            {formatParagraph(value.description, index)}
-            {renderButtons(value, index)}
+
+            {splitDescription(value.description).map((text, i) => (
+              formatParagraph(text, i)
+            ))}
+
+  
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={() => {
+                props.handleSave(value);
+                const updatedIsSaved = [...isSaved];
+                updatedIsSaved[index] = !updatedIsSaved[index];
+                setIsSaved(updatedIsSaved);
+              }}
+              className={`${styles.saveButton} ${isSaved[index] ? styles.savedButton : ""}`}
+              disabled={isSaved[index]}
+            >
+              {isSaved[index] ? "Saved!" : "Save this Job!"}
+
+            </Button>
+
+            <Button
+              onClick={() => props.generateCL(value.title, value.description)}
+              variant="primary"
+            >
+              Generate a Cover Letter!
+            </Button>
           </Card.Body>
         </Card>
       ))}
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from "./Components/styles/Footer.module.css";
 
 const Footer = () => {
   return (
-      <div className="footerItems">
+      <div className={styles.footerItems}>
         <img
-          alt=""
+          alt="Pawgrammers collective logo"
           src="./img/logo.png"
 
         />
