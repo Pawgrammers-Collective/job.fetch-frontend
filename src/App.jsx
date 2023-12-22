851df928f73d106fb643c484596c67432cf18c0d
JohnnyBackus <john.p.backus@gmail.com>
2023-12-22 14:53:24 -0800

    changes made to clean up App and fix spinner on job card before pull from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,19 +10,106 @@ import UserProfile from './Components/UserProfile.jsx';
 import Footer from './Footer.jsx'
 import Header from './Header.jsx'
 import LandingPage from './Components/LandingPage';
-import SearchForm from './Components/SearchForm.jsx';
 import { withAuth0 } from '@auth0/auth0-react';
 
 const url = import.meta.env.VITE_SERVER_URL;
 
 function App(props) {
-  const [city, setCity] = useState('');
   const [jobs, setJobs] = useState([]);
   const [savedJobs, setSavedJobs] = useState([]);
+  const [genCLJobDesc, setGenCLJobDesc] = useState('');
   const [coverLetter, setCoverLetter] = useState([]);
   const [savedCLs, setSavedCLs] = useState([]);
-  const [genCLJobDesc, setGenCLJobDesc] = useState('');
   const [newsArticle, setNewsArticle] = useState({});
+
+  // --------------- job functions ----------------
+
+  async function handleJobSearch(city) {
+    console.log(url);
+    console.log(city);
+    if (props.auth0.isAuthenticated) {
+      let claim = await props.auth0.getIdTokenClaims();
+      console.log(claim)
+      let token = claim.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "get",
+        url: `${url}/jobs?location=${city}`
+      }
+      try {
+        let response = await axios(config);
+        console.log(response.data);
+        setJobs(response.data);
+      } catch (error) {
+        console.error('Error getting jobs:', error.message);
+      }
+    }
+  }
+
+  async function saveJob(job) {
+    console.log(job);
+    if (props.auth0.isAuthenticated) {
+      let claim = await props.auth0.getIdTokenClaims();
+      console.log(claim)
+      let token = claim.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "post",
+        url: `${url}/jobs`,
+        data: job
+      }
+      try {
+        let response = await axios(config);
+        console.log("Server Response", response.data);
+        // setSavedJobs(... savedJobs , response.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
+
+  async function getSavedJobs() {
+    if (props.auth0.isAuthenticated) {
+      let claim = await props.auth0.getIdTokenClaims();
+      console.log(claim)
+      let token = claim.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "get",
+        url: `${url}/jobs/saved`,
+      }
+      try {
+        let response = await axios(config);
+        console.log(response.data);
+        setSavedJobs(response.data);
+      } catch (error) {
+        console.error('Error getting jobs:', error.message);
+      }
+    }
+  }
+
+  async function deleteSavedJob(job) {
+    console.log('deleting ', { job });
+    if (props.auth0.isAuthenticated) {
+      let claim = await props.auth0.getIdTokenClaims();
+      console.log(claim)
+      let token = claim.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "delete",
+        url: `${url}/jobs/${job._id}`,
+      }
+      try {
+        let response = await axios(config);
+        console.log(response.data);
+        getSavedJobs();
+      } catch (error) {
+        console.error('Error deleting jobs:', error.message);
+      }
+    }
+  }
+
+  // --------------- cover letter functions ----------------
 
   async function generateCL(jobTitle, jobDescription) {
     if (props.auth0.isAuthenticated) {
@@ -33,7 +120,6 @@ function App(props) {
         headers: { "Authorization": `Bearer ${token}` },
         method: "get",
         url: `${url}/cover`,
-        // If this doesn't work prob the params
         params: {
           jobTitle,
           jobDescription,
@@ -97,127 +183,6 @@ function App(props) {
     }
   }
 
-  async function getInterviewQuestions(request, response) {
-    if (props.auth0.isAuthenticated) {
-      let claim = await props.auth0.getIdTokenClaims();
-      console.log(claim)
-      let token = claim.__raw;
-      const config = {
-        headers: { "Authorization": `Bearer ${token}` },
-        method: "get",
-        url: `${url}/interview`,
-      }
-      try {
-        response = await axios(config);
-        console.log(response.data);
-        // setInterviewQuestion(response.data)
-      } catch (e) {
-        console.log('frontend interview questions no worky :(', e)
-      }
-    }
-  }
-
-
-
-  function handleSearch(searchInput) {
-    console.log(searchInput);
-    setCity(searchInput);
-    getJobs(searchInput);
-  }
-
-
-    async function getJobs(city) {
-      console.log(url);
-      if (props.auth0.isAuthenticated) {
-        let claim = await props.auth0.getIdTokenClaims();
-        console.log(claim)
-        let token = claim.__raw;
-        const config = {
-          headers: { "Authorization": `Bearer ${token}` },
-          method: "get",
-          url: `${url}/jobs?location=${city}`
-        }
-        try {
-          let response = await axios(config);
-          console.log(response.data);
-          setJobs(response.data);
-        } catch (error) {
-          console.error('Error getting jobs:', error.message);
-        }
-      }
-    }
-
-
-    function handleSave(job) {
-      console.log(job);
-      // setSavedJobs(job);
-      saveJob(job);
-    }
-
-    async function saveJob(job) {
-      console.log(job);
-      if (props.auth0.isAuthenticated) {
-        let claim = await props.auth0.getIdTokenClaims();
-        console.log(claim)
-        let token = claim.__raw;
-        const config = {
-          headers: { "Authorization": `Bearer ${token}` },
-          method: "post",
-          url: `${url}/jobs`,
-          data: job
-        }
-        try {
-          let response = await axios(config);
-          console.log("Server Response", response.data);
-          // setSavedJobs(... savedJobs , response.data);
-        } catch (error) {
-          console.error(error.message);
-        }
-      }
-    }
-
-    async function getSavedJobs() {
-      if (props.auth0.isAuthenticated) {
-        let claim = await props.auth0.getIdTokenClaims();
-        console.log(claim)
-        let token = claim.__raw;
-        const config = {
-          headers: { "Authorization": `Bearer ${token}` },
-          method: "get",
-          url: `${url}/jobs/saved`,
-        }
-        try {
-          let response = await axios(config);
-          console.log(response.data);
-          setSavedJobs(response.data);
-        } catch (error) {
-          console.error('Error getting jobs:', error.message);
-        }
-      }
-    }
-
-
-  async function deleteSavedJob(job) {
-    console.log('deleting ', { job });
-    if (props.auth0.isAuthenticated) {
-      let claim = await props.auth0.getIdTokenClaims();
-      console.log(claim)
-      let token = claim.__raw;
-      const config = {
-        headers: { "Authorization": `Bearer ${token}` },
-        method: "delete",
-        url: `${url}/jobs/${job._id}`,
-      }
-      try {
-        let response = await axios(config);
-        console.log(response.data);
-        getSavedJobs();
-      } catch (error) {
-        console.error('Error deleting jobs:', error.message);
-      }
-    }
-  }
-
   async function deleteSavedCL(cover) {
     console.log('deleting ', { cover });
     if (props.auth0.isAuthenticated) {
@@ -239,6 +204,8 @@ function App(props) {
     }
   }
 
+  //---------------- news functions ------------------- 
+
   async function getNews(company) {
     let claim = await props.auth0.getIdTokenClaims();
     console.log(claim)
@@ -259,6 +226,28 @@ function App(props) {
     }
   }
 
+  // -------stretch goal interview questions functions --------
+  
+  async function getInterviewQuestions(request, response) {
+    if (props.auth0.isAuthenticated) {
+      let claim = await props.auth0.getIdTokenClaims();
+      console.log(claim)
+      let token = claim.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "get",
+        url: `${url}/interview`,
+      }
+      try {
+        response = await axios(config);
+        console.log(response.data);
+        // setInterviewQuestion(response.data)
+      } catch (e) {
+        console.log('frontend interview questions no worky :(', e)
+      }
+    }
+  }
+
   return (
     <>
       <Router>
@@ -274,12 +263,12 @@ function App(props) {
               exact path="/Home"
               element={<Home
                 jobs={jobs}
+                saveJob={saveJob}
                 generateCL={generateCL}
                 genCLJobDesc={genCLJobDesc}
                 saveCL={saveCL}
                 coverLetter={coverLetter}
-                handleSave={handleSave}
-                handleSearch={handleSearch}
+                handleSearch={handleJobSearch}
                 getQuestions={getInterviewQuestions}
                 getNews={getNews}
                 newsArticle={newsArticle}
@@ -311,4 +300,3 @@ function App(props) {
   }
 
   export default withAuth0(App);
-
--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,38 +1,52 @@
-/* eslint-disable react/prop-types */
 import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Spinner from 'react-bootstrap/Spinner';
 
 function SearchForm(props) {
   const { isAuthenticated } = useAuth0();
   const [show, setShow] = useState(false);
   const [cityInput, setCityInput] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+  
   function handleChange(e) {
     setCityInput(e.target.value);
     console.log(e);
   }
-
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    props.handleSearch(cityInput);
+    setIsLoading(true);
     handleClose();
+    await props.handleSearch(cityInput);
+    setIsLoading(false);
     console.log(cityInput);
   }
-
   return (
     <>
       {isAuthenticated ? (
         <>
           <Button variant="primary" onClick={handleShow}>
-            Launch job search
+          {isLoading ? (
+                <>
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                    variant="success"
+                  />
+                  <span>Fetching your jobs...</span>
+                </>
+              ) : (
+                'Launch job search'
+              )}
           </Button>
-
           <Modal
             show={show}
             onHide={handleClose}
@@ -43,7 +57,7 @@ function SearchForm(props) {
               <Modal.Title>Job location search</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <Form onSubmit={handleSubmit}>
+              <Form>
                 <InputGroup className="mb-3">
                   <Form.Control
                     type="text"
@@ -56,7 +70,6 @@ function SearchForm(props) {
                   <Button
                     variant="outline-secondary"
                     id="search-button"
-                    type="submit"
                     onClick={handleSubmit}
                   >
                     Search
@@ -77,5 +90,4 @@ function SearchForm(props) {
     </>
   );
 }
-
 export default SearchForm;
